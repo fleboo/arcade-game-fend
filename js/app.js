ea5a147f448ecc1ebfea39768cfafc8477bd6b0e
Felix Leboo <felixlemi@gmail.com>
2018-07-29 12:19:46 -0500

    Added Player class

--- js/app.js
+++ js/app.js
@@ -36,6 +36,11 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+let Player = function(x, y) {
+    this.x = x;
+    this.y = y;
+    this.player = 'images/char-boy.png';
+};
 
 
 // Now instantiate your objects.
